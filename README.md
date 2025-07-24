1851d7460c45105f2add17e71a307649bdfe9c60
carline <karlijnkools13@outlook.com>
2025-07-25 01:25:25 +0200

    day two

--- README.md
+++ README.md
@@ -32,3 +32,41 @@ Make a new commit with the name "make navbar cool"
 ```sh
 git commit -m "make navbar cool"
 ```
+
+## Day 2
+
+### Starting Vite
+
+So I can actually see what I'm working on, write this in terminal and Vite will open.
+
+```sh
+pnpx vite
+```
+### Google Fonts
+
+Choose a cute font on Google Fonts and get embed code. Copy and paste code into css file and have happy little fonts.
+
+```sh
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bitcount:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+```
+
+I copied this into my html head element.
+
+```sh
+
+.bitcount {
+  font-family: "Bitcount", system-ui;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  font-variation-settings:
+    "slnt" 0,
+    "CRSV" 0.5,
+    "ELSH" 0,
+    "ELXP" 0;
+}
+```
+I put this code in my css file and I got pretty font! :).
+
---
+++ day-2/index.js
@@ -0,0 +1,46 @@
+// add new input
+// cross listitem of list when clicked
+
+let lucBtn = document.querySelector('#luc-button');
+
+lucBtn.addEventListener('click', function () {
+    // alert('boobs');
+    console.log('smol bobber');
+});
+
+let input = document.querySelector('#myInput');
+let myUl = document.querySelector('#myUl');
+
+let addButton = document.querySelector('.addButton');
+
+addButton.addEventListener('click', function () {
+    let x = input.value;
+    input.value = '';
+
+
+
+    if (x.trim().length == 0) {
+        alert("dont be dumb");
+        return;
+    }
+
+    console.log("addddd" + x);
+
+    //
+    let myItem = document.createElement("li");
+    myItem.innerText = x;
+    addStrikeThroughListener(myItem);
+    myUl.appendChild(myItem);
+});
+
+function addStrikeThroughListener(element) {
+    element.addEventListener("click", function () {
+        element.classList.toggle("checked")
+    });
+}
+
+const allItems = document.querySelectorAll("ul li");
+
+for (const item of allItems) {
+    addStrikeThroughListener(item);
+}
