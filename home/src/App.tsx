9624d195e962337d6c4b8147835367f2d85996e6
carline <carline@v3x.email>
2025-10-23 19:14:25 +0200

    tailwind

--- home/src/App.tsx
+++ home/src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
     <>
-      <h1>hello</h1>
-      <a href="/100-days-of-code/hangman/">Day 7: hangman</a>
+      <div className="border p-4 bg-pink-500">
+        <h1>hello</h1>
+        <a href="/100-days-of-code/hangman/">Day 7: hangman</a>
+      </div>
     </>
   );
 }
