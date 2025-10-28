c2e4851d0d0d53bf034cd5649e6c38426f5a6bba
carline <carline@v3x.email>
2025-10-28 16:35:27 +0100

    update vite.config

--- day-010/vite.config.ts
+++ day-010/vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/100-days-of-code/day-010/",
 })
