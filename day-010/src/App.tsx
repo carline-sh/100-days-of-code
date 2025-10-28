1bf02232f2e5b6ae53f3dbf611226e0a714908df
carline <carline@v3x.email>
2025-10-29 00:18:11 +0100

    fix password

--- day-010/src/App.tsx
+++ day-010/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
               <input type="text" placeholder="E-mail" />
             </div>
             <div className="p-2 border m-4 rounded-md bg-neutral-100 border-neutral-400">
-              <input type="text" placeholder="Password" />
+              <input type="password" placeholder="Password" />
             </div>
             <div>
               <button className="border rounded-md p-2 m-4 mt-1 text-neutral-800 border-neutral-500 w-60 bg-neutral-300 hover:bg-neutral-400">
