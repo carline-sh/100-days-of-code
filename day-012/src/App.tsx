58659c916e22ae10a791187e489ebf14f0e961db
carline <carline@v3x.email>
2025-11-03 23:20:03 +0100

    update

--- day-012/src/App.tsx
+++ day-012/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <div className="grid grid-cols-[1fr_3fr_1fr]">
         <Navbar />
         <Content />
+        <div className="bg-blue-500">tailrain</div>
       </div>
     </div>
   );
